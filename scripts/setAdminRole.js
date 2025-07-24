fa98ee78ecc2abca33e6565606ffe55f4fe6dd1e
AdinAdryTjindarbumi <adin.122140024@student.itera.ac.id>
2025-07-24 12:50:41 +0700

    Web Magnet BPS Balam

--- scripts/setAdminRole.js
+++ scripts/setAdminRole.js
@@ -3,20 +3,20 @@ const { getAuth } = require("firebase-admin/auth");
 const serviceAccount = require("../serviceAccountKey.json");
 
 initializeApp({
-    credential: cert(serviceAccount),
+  credential: cert(serviceAccount),
 });
 
 const setAdminRole = async () => {
-    try {
-        const user = await getAuth().getUserByEmail("pembimbing@gmail.com");
-        await getAuth().setCustomUserClaims(user.uid, { role: "pembimbing" });
-        console.log(`Role pembimbing berhasil di-set untuk ${user.email}`);
-        // const user = await getAuth().getUserByEmail("bps1871@gmail.com");
-        // await getAuth().setCustomUserClaims(user.uid, { role: "admin" });
-        // console.log(`Role admin berhasil di-set untuk ${user.email}`);
-    } catch (error) {
-        console.error("Gagal set role:", error.message);
-    }
+  try {
+    const user = await getAuth().getUserByEmail("pembimbing@gmail.com");
+    await getAuth().setCustomUserClaims(user.uid, { role: "pembimbing" });
+    console.log(`Role pembimbing berhasil di-set untuk ${user.email}`);
+    // const user = await getAuth().getUserByEmail("bps1871@gmail.com");
+    // await getAuth().setCustomUserClaims(user.uid, { role: "admin" });
+    // console.log(`Role admin berhasil di-set untuk ${user.email}`);
+  } catch (error) {
+    console.error("Gagal set role:", error.message);
+  }
 };
 
 setAdminRole();
